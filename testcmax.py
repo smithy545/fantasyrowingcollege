c3b2b0d16a1cb3ce4536578b4d73fd39abdd4c4b
smithy545 <smithy545@gmail.com>
2015-11-26 13:14:18 -0500

    Adjusted plotting functions; Looks to be in line with what you'd expect;

--- testcmax.py
+++ testcmax.py
@@ -5,11 +5,13 @@ for cmax in range(0, 20):
     winner = []
     loser = []
     for mov in range(0, 60):
-        winner.append((mov - cmax + 10)*2)
-        loser.append((cmax - mov + 2)*2)
+        winner.append((mov - cmax + 10))
+        loser.append((cmax - mov + 2))
     plt.figure(1)
-    plt.plot(range(0,60),winner)
-    plt.plot(range(0,60),loser)
+    plt.xlabel('MOV - dCMAX')
+    plt.ylabel('Points earned')
+    plt.plot([x - cmax for x in range(0,60)],winner, 'rx')
+    plt.plot([x - cmax for x in range(0,60)],loser, 'bo')
 
 plt.show()
 
@@ -18,10 +20,12 @@ for cmax in range(0, 20):
     winner = []
     loser = []
     for mov in range(0, 60):
-        winner.append(-2*(mov + cmax + 10))
-        loser.append((cmax + mov + 5)*2)
+        winner.append(-(mov + cmax + 10))
+        loser.append((cmax + mov + 5))
     plt.figure(2)
-    plt.plot(range(0,60),winner)
-    plt.plot(range(0,60),loser)
+    plt.xlabel('MOV - dCMAX')
+    plt.ylabel('Points earned')
+    plt.plot([x - cmax for x in range(0,60)],winner, 'rx')
+    plt.plot([x - cmax for x in range(0,60)],loser,'bo')
 
 plt.show()
